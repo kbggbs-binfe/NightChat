b0081a402d7ba0fc9f3ce3c2cd9967833cc4946e
kbggbs-binfe <cubingwithjaffar2526@gmail.com>
2026-08-11 21:30:18 +0530

    Add NightChat WebView activity

---
+++ android/app/src/main/java/com/kbggbs/nightchat/MainActivity.java
@@ -0,0 +1,39 @@
+package com.kbggbs.nightchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://nightchat-pre9.onrender.com");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
